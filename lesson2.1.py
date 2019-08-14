bc3c5ade4bd2db500159d3c551cd5308ce7d0583
AlferevDA <alferev_1991@mail.ru>
2019-08-14 06:33:40 +0300

    Сделал работу с урока.

--- lesson2.1.py
+++ lesson2.1.py
@@ -1,16 +1,29 @@
-#-------------------------------------------------------------------------------
-# Name:        lesson2
-# Purpose:
-#
-# Author:      Алферьев Дмитрий Александрович
-#
-# Created:     13.08.2019
-# Copyright:   (c) Дмитрий 2019
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
+#from pprint import pprint
+#import re
+#import requests
+#import json
+#req = requests.get("https://yandex.ru").text
+#temp = re.findall("([-+][0-9]+)°",req)
+#print(temp)
 
-def main():
-    pass
+#cities = ['Moscow', 'Novosibirsk', 'Anapa', 'Kazan', 'Tomsk', 'Vladivostok']
+#appid = 'e91a6d9a3c9d15102a89ac457b045ec2'
+#main_link = 'http://api.openweathermap.org/data/2.5/weather?'
+#for city in cities:
+#    link = f'{main_link}q={city}&appid={appid}'
+#    req = requests.get(link)
+#    data = json.loads(req.text)
+#    print(data['name'],data['main']['temp']-273.15)
 
-if __name__ == '__main__':
-    main()
+from pprint import pprint
+import requests
+import json
+link = 'http://min-prices.aviasales.ru/calendar_preload?'
+origin = 'MOW' #IATA == Moscow -> MOW
+destination = 'LED' #IATA == LED
+link = f'{link}origin={origin}&destination={destination}'
+req = requests.get(link)
+data = json.loads(req.text)
+pprint(data)
+for i in data['best_prices']:
+    print(i['value'],i['depart_date'],i['gate'])
