84a2f5f00d8129fe692a0ee8cb1fa827ddf2212b
AlferevDA <alferev_1991@mail.ru>
2019-08-14 06:34:34 +0300

    Сделал работу с урока, но почему-то не работает.

--- lesson2.2.py
+++ lesson2.2.py
@@ -1,16 +1,33 @@
-#-------------------------------------------------------------------------------
-# Name:        lesson2
-# Purpose:
-#
-# Author:      Алферьев Дмитрий Александрович
-#
-# Created:     13.08.2019
-# Copyright:   (c) Дмитрий 2019
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
+import requests
+import re
+from pprint import pprint
 
-def main():
-    pass
+def get_link(topic):
+    link='https://ru.wikipedia.org/wiki/'+topic.capitalize()
+    return link
 
-if __name__ == '__main__':
-    main()
+def get_topic_page(topic):
+    link = get_link(topic)
+    html_content = requests.get(link).text
+    # with open('new.html','w', encoding='utf-8') as f:
+    #     f.write(html_content)
+    return html_content
+
+def get_topic_text(topic):
+    html_content = get_topic_page(topic)
+    words = re.findall('[а-яА-Я]{3,}',html_content)
+    return words
+
+def get_common_words(topic):
+    word_list = get_topic_text(topic)
+    rate=()
+    for word in word_list:
+        if word in rate:
+            rate[word] +=1
+        else:
+            rate[word] = 1
+    rate_list = list(rate.items())
+    rate_list.sort(key = lambda x: -x[1]) # - это сортировка от большего к меньшему
+    return rate_list
+
+pprint(get_common_words('Дерево')[:10])
