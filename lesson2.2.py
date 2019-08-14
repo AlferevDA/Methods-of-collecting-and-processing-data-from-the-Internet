10081d346963ac73d1be36b03fa90e1787d049e1
AlferevDA <alferev_1991@mail.ru>
2019-08-14 10:45:48 +0300

    Второе домашнее задание.

--- lesson2.2.py
+++ lesson2.2.py
@@ -1,6 +1,8 @@
 import requests
 import re
 from pprint import pprint
+import json
+from collections import Counter
 
 def get_link(topic):
     link='https://ru.wikipedia.org/wiki/'+topic.capitalize()
@@ -30,4 +32,36 @@ def get_common_words(topic):
     rate_list.sort(key = lambda x: -x[1]) # - это сортировка от большего к меньшему
     return rate_list
 
-pprint(get_common_words('Футбол')[:10])
+# pprint(get_common_words('Футбол')[:10])
+
+# В приложении парсинга википедии получить первую ссылку из раздела "Ссылки" и
+# вывести все значимые слова из неё. Результат записать в файл в
+# форматированном виде.
+
+def get_first_linked_page(topic, n_words=10):
+    html_content = get_topic_page(topic)
+    try:
+        text = re.findall(r'<li><a rel=.+\sclass=.+\shref=\"https?://\S+?\">', html_content)
+        for idx, line in enumerate(text[:1]):
+            link = re.findall(r'"((http)s?://.*?)"', line)[0][0]
+            try:
+                html = requests.get(link, timeout=5).text
+                words = re.findall(r'[а-яА-Я]{3,}', html)
+                counter = Counter(words)
+                most_common_words = dict(counter.most_common(n_words))
+                file_name = topic.capitalize() + '_' + '0' * (4 - len(str(idx))) + str(idx+1) + '.json'
+                with open(file_name, 'w', encoding='utf-8') as f:
+                    f.write(link)
+                with open(file_name, 'a', encoding='utf-8') as f:
+                    json.dump(most_common_words, f, ensure_ascii=False)
+                print(f'Ссылка №{idx+1}: {link}')
+                print(f'{n_words} значимых слов:')
+                pprint(most_common_words)
+                print(f'Data saved to: {file_name}')
+                print('\n')
+            except:
+                continue
+    except:
+        pass
+
+get_first_linked_page('Ломоносов')
