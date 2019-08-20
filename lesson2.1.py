2c00042d5fa58176c2eec161d94ef827ef03da24
AlferevDA <alferev_1991@mail.ru>
2019-08-20 15:47:58 +0300

    Доделал и разобрался полностью.

--- lesson2.1.py
+++ lesson2.1.py
@@ -1,36 +1,47 @@
-from pprint import pprint
-import re
-import requests
-import json
+import requests   # библиотека для получения инф. из интернета :)
+import re   # библиотека для поиска по полученным данным
+
+import json   # какая-то штука для преобразования
+
+from pprint import pprint   # модуль для более удобного отображения информации (словарь со словарями)
+
 import pandas as pd
 
 # Задача 1 (Данные о погоде)
+
+# Задача 1.1 (Температура воздуха со стартовой страницы "Yandex"
 '''
 req = requests.get("https://yandex.ru").text
-temp = re.findall("([-+][0-9]+)°",req)
+temp = re.findall("([-+][0-9]+)°", req)
 print(temp)
+'''
 
-cities = ['Moscow', 'Novosibirsk', 'Anapa', 'Kazan', 'Tomsk', 'Vladivostok']
-appid = 'e91a6d9a3c9d15102a89ac457b045ec2'
+# Задача 1.2 (Температура воздуха в указанных городах с ресурса "OpenWeatherMap"
+'''
 main_link = 'http://api.openweathermap.org/data/2.5/weather?'
+appid = 'e91a6d9a3c9d15102a89ac457b045ec2'
+cities = ['Moscow', 'Novosibirsk', 'Anapa', 'Kazan', 'Tomsk', 'Vladivostok']
+
 for city in cities:
     link = f'{main_link}q={city}&appid={appid}'
     req = requests.get(link)
     data = json.loads(req.text)
-    print(data['name'],data['main']['temp']-273.15)
+    print(data['name'], data['main']['temp'] - 273.15)
 '''
+
 # Задача 2 (Авиалинии)
 '''
-#link = 'http://min-prices.aviasales.ru/calendar_preload?'
-#origin = 'MOW' #IATA == Moscow -> MOW
-#destination = 'LED' #IATA == LED
-#link = f'{link}origin={origin}&destination={destination}'
-#req = requests.get(link)
-#data = json.loads(req.text)
-#pprint(data)
-#for i in data['best_prices']:
-#    print(i['value'],i['depart_date'],i['gate'])
+main_link = 'http://min-prices.aviasales.ru/calendar_preload?'
+origin_IATA = 'MOW' #IATA == Moscow -> MOW
+destination_IATA = 'LED' #IATA == ‎Saint Petersburg -> LED
+link = f'{main_link}origin={origin_IATA}&destination={destination_IATA}'
+req = requests.get(link)
+data = json.loads(req.text)
+pprint(data) # это для изучения того, что нам вытащить
+for i in data['best_prices']:
+    print(i['value'], i['depart_date'], i['gate'])
 '''
+
 # Доработать приложение по поиску авиабилетов, чтобы оно возвращало билеты по
 # названию города, а не по IATA коду. (У aviasales есть для этого
 # дополнительное API) Пункт отправления и пункт назначения должны передаваться
@@ -38,60 +49,30 @@ for city in cities:
 # пункт отправления, пункт назначения, дату вылета, цену билета (можно добавить
 # еще другие параметры по желанию)
 
-def get_cities():
+origin = input('Город отправления: ')
+destination = input('Город прибытия: ')
 
-    link = 'http://api.travelpayouts.com/data/ru/cities.json'
+iata_link = f'https://www.travelpayouts.com/widgets_suggest_params?q=Из%20{origin}%20в%20{destination}'
+iata_req = requests.get(iata_link)
+iata_data = json.loads(iata_req.text)
+origin_IATA = iata_data['origin']['iata']
+destination_IATA = iata_data['destination']['iata']
 
-    with requests.session() as s:
-        r = s.get(link)
+main_link = 'http://min-prices.aviasales.ru/calendar_preload?'
+#origin_IATA = 'MOW' #IATA == Moscow -> MOW
+#destination_IATA = 'LED' #IATA == ‎Saint Petersburg -> LED
+link = f'{main_link}origin={origin_IATA}&destination={destination_IATA}'
+req = requests.get(link)
+data = json.loads(req.text)
 
-    data = json.loads(r.text)
+feature_names = ['depart_date', 'value']
 
-    cities = dict()
+tickets = pd.DataFrame(data['best_prices'])[feature_names]
 
-    for item in data:
-        cities[item['name']] = item['code']
-        cities[item['name_translations']['en']] = item['code']
-
-    return cities
-
-def get_tickets(origin_name, destination_name, one_way_ticket=True):
-
-    cities = get_cities()
-
-    origin = cities.get(origin_name, '--')
-    destination = cities.get(destination_name, '--')
-
-    ticket_type = {True: 'true', False: 'false'}
-
-    one_way = ticket_type.get(one_way_ticket, True)
-
-    service = 'http://min-prices.aviasales.ru/calendar_preload?'
-
-    link = f'{service}origin={origin}&destination={destination}&one_way={one_way}'
-
-    with requests.session() as s:
-        r = s.get(link)
-
-    data = json.loads(r.text)
-
-    feature_names = ['origin', 'destination', 'depart_date', 'value']
-
-    tickets = pd.DataFrame(data['best_prices'])[feature_names]
-    tickets['origin'] = origin_name
-    tickets['destination'] = destination_name
-
-    for idx, row in tickets.iterrows():
-
-        print('*' * 36)
-        print(f'№:                  {idx+1}')
-        print(f'Пункт отправления:  {row[0]}')
-        print(f'Пункт назначения:   {row[1]}')
-        print(f'Дата отправления:   {row[2]}')
-        print(f'Цена билета:        {row[3]}')
-        print('*' * 36)
-        print('\n')
-
-    return tickets
-
-tickets = get_tickets('Череповец', 'Москва', one_way_ticket=False)
+for idx, row in tickets.iterrows():
+    print('*' * 36)
+    print(f'№:                  {idx+1}')
+    print(f'Пункт отправления:  {origin}')
+    print(f'Пункт назначения:   {destination}')
+    print(f'Дата отправления:   {row[0]}')
+    print(f'Цена билета:        {row[1]}')
